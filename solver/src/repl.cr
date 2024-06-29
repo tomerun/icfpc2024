f053b6255aa165b31fd941c6883f3222a9eddf1f
tomerun <tomerunmail@gmail.com>
2024-06-29 23:44:52 +0900

    repl raw

--- solver/src/repl.cr
+++ solver/src/repl.cr
@@ -1,3 +1,4 @@
+require "option_parser"
 require "api"
 require "common"
 
@@ -13,36 +14,61 @@ def get(api, input)
     prog.print(STDOUT, 0)
   end
   puts "----finish eval----"
-  return prog.to_s
+  return prog
 end
 
 def main
+  raw = false
+  parser = OptionParser.new do |parser|
+    parser.on("-r", "raw") { raw = true }
+  end
+
+  parser.parse
+
   api = API.new
-  while true
-    input = [] of Char
-    buf = [] of Char
-    in_raw = false
-    STDIN.read_line.each_char do |ch|
-      if ch.ord < 128
-        if in_raw
-          input << ch
-        else
-          buf << ch
-        end
+  begin
+    while true
+      input = [] of Char
+      buf = [] of Char
+      in_raw = false
+      line = STDIN.read_line
+      if raw
+        body = line.strip
       else
-        in_raw = !in_raw
+        line.each_char do |ch|
+          if ch.ord < 128
+            if in_raw
+              input << ch
+            else
+              buf << ch
+            end
+          else
+            in_raw = !in_raw
+            if !buf.empty?
+              input += StringT.convert(buf)
+              buf.clear
+            end
+          end
+        end
         if !buf.empty?
           input += StringT.convert(buf)
           buf.clear
         end
+        body = "S" + input.join
+      end
+      output = get(api, body)
+      case output
+      when StringT
+        puts output.s
+      when IntegerT
+        puts output.v
+      when TrueT
+        puts true
+      when FalseT
+        puts false
       end
     end
-    if !buf.empty?
-      input += StringT.convert(buf)
-      buf.clear
-    end
-    output = get(api, "S" + input.join)
-    puts output
+  rescue IO::EOFError
   end
 end
 
