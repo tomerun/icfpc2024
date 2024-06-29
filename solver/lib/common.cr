fc8cf7f47a53deee836542f82a22dfc1c42c5dba
tomerun <tomerunmail@gmail.com>
2024-06-29 14:30:06 +0900

    can evaluate the program in "Limits" section

--- solver/lib/common.cr
+++ solver/lib/common.cr
@@ -1,50 +1,182 @@
 STR_TBL     = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
 STR_TBL_REV = Array.new(STR_TBL.size) { |i| v = STR_TBL.index('!' + i); v ? '!' + v : ' ' }
 
-class TrueT
-  def eval
+macro debug(msg)
+  {% if flag?(:local) %}
+    STDERR.puts({{msg}})
+  {% end %}
+end
+
+macro debugf(format_string, *args)
+  {% if flag?(:local) %}
+    STDERR.printf({{format_string}}, {{*args}})
+  {% end %}
+end
+
+def crash(msg, caller_line = __LINE__)
+  STDERR.puts "[ERROR] line #{caller_line}: #{msg}"
+  exit
+end
+
+macro assert(cond, msg = "", caller_line = __LINE__)
+  {% if flag?(:local) %}
+    if !({{cond}})
+      crash({{msg}}, {{caller_line}})
+    end
+  {% end %}
+end
+
+class VarsIdx
+  property :map, :idx
+
+  def initialize
+    @map = Hash(Int64, Int64).new
+    @idx = 0i64
+  end
+end
+
+class Ctx
+  getter :bounds, :unbounds
+
+  def initialize
+    @bounds = Hash(Int64, Term).new
+    @unbounds = Set(Int64).new
+  end
+end
+
+class CloneCtx
+  getter :exist, :map
+
+  def initialize(@exist : Set(Int64))
+    @map = Hash(Int64, Int64).new
+    @max_key = @exist.max
+  end
+
+  def get(vi : Int64)
+    if @map.has_key?(vi)
+      @map[vi]
+    elsif @exist.includes?(vi)
+      @max_key += 1
+      @map[vi] = @max_key
+      @max_key
+    else
+      @map[vi] = vi
+      vi
+    end
+  end
+end
+
+module Arity0
+  def clone(cctx)
     self
   end
+
+  def eval(ctx)
+    self
+  end
+
+  def rename_vars(idx)
+  end
+
+  def substitute(vi, term, ctx)
+    self
+  end
+end
+
+module Arity1
+  def initialize(@t : Term)
+  end
+
+  def clone(cctx)
+    self.class.new(@t.clone(cctx))
+  end
+
+  def rename_vars(idx)
+    @t.rename_vars(idx)
+  end
+
+  def substitute(vi, term, ctx)
+    @t = @t.substitute(vi, term, ctx)
+    self
+  end
+end
+
+module Arity2
+  def initialize(@t0 : Term, @t1 : Term)
+  end
+
+  def clone(cctx)
+    self.class.new(@t0.clone(cctx), @t1.clone(cctx))
+  end
+
+  def rename_vars(idx)
+    @t0.rename_vars(idx)
+    @t1.rename_vars(idx)
+  end
+
+  def substitute(vi, term, ctx)
+    @t0 = @t0.substitute(vi, term, ctx)
+    @t1 = @t1.substitute(vi, term, ctx)
+    self
+  end
+end
+
+class TrueT
+  include Arity0
 
   def to_s(io)
     io << true
   end
+
+  def print(io, depth)
+    io << "  " * depth << "true\n"
+  end
 end
 
 class FalseT
-  def eval
-    self
-  end
+  include Arity0
 
   def to_s(io)
     io << false
   end
+
+  def print(io, depth)
+    io << "  " * depth << "false\n"
+  end
 end
 
 class IntegerT
+  include Arity0
+  @v : Int64
   getter :v
-
-  def initialize(s : String)
-    @v = 0i64
-    s.each_char do |ch|
-      @v *= 94
-      @v += ch - '!'
-    end
-  end
 
   def initialize(@v : Int64)
   end
 
-  def eval
-    self
+  def initialize(s : String)
+    @v = IntegerT.parse(s)
+  end
+
+  def self.parse(s : String)
+    v = 0i64
+    s.each_char do |ch|
+      v *= 94
+      v += ch - '!'
+    end
+    v
   end
 
   def to_s(io)
     io << @v
   end
+
+  def print(io, depth)
+    io << "  " * depth << @v << "\n"
+  end
 end
 
 class StringT
+  include Arity0
   getter :s
 
   def initialize(s : String, raw = false)
@@ -63,78 +195,127 @@ class StringT
     self.convert(s.chars).join
   end
 
-  def eval
-    self
-  end
-
   def to_s(io)
     io << @s
+  end
+
+  def print(io, depth)
+    io << "  " * depth << @s << "\n"
   end
 end
 
 class NegT
-  def initialize(@t : Term)
+  include Arity1
+
+  def eval(ctx)
+    v = @t.eval(ctx)
+    if v.is_a?(IntegerT)
+      IntegerT.new(-v.v)
+    else
+      self.class.new(v)
+    end
   end
 
-  def eval
-    IntegerT.new(-@t.eval.as(IntegerT).v)
+  def to_s(io)
+    io << "- " << @t
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "-\n"
+    @t.print(io, depth + 1)
   end
 end
 
 class NotT
-  def initialize(@t : Term)
-  end
+  include Arity1
 
-  def eval
-    v = @t.eval
+  def eval(ctx)
+    v = @t.eval(ctx)
     if v.is_a?(TrueT)
       FalseT.new
     elsif v.is_a?(FalseT)
       TrueT.new
     else
-      raise "invalid type"
+      self.class.new(v)
     end
+  end
+
+  def to_s(io)
+    io << "! " << @t
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "!\n"
+    @t.print(io, depth + 1)
   end
 end
 
 class S2IT
-  def initialize(@t : Term)
+  include Arity1
+
+  def eval(ctx)
+    v = @t.eval(ctx)
+    if v.is_a?(StringT)
+      i = 0i64
+      v.s.each_char do |ch|
+        i *= 94
+        i += STR_TBL_REV[ch - '!'] - '!'
+      end
+      IntegerT.new(i)
+    else
+      self.class.new(v)
+    end
   end
 
-  def eval
-    v = @t.eval
-    i = 0i64
-    v.as(StringT).s.each_char do |ch|
-      i *= 94
-      i += STR_TBL_REV[ch - '!'] - '!'
-    end
-    IntegerT.new(i)
+  def to_s(io)
+    io << "# " << @t
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "#\n"
+    @t.print(io, depth + 1)
   end
 end
 
 class I2ST
-  def initialize(@t : Term)
+  include Arity1
+
+  def eval(ctx)
+    v = @t.eval(ctx)
+    if v.is_a?(IntegerT)
+      i = v.v
+      s = [] of Char
+      while i > 0
+        s << STR_TBL[i % 94]
+        i //= 94
+      end
+      StringT.new(s.reverse.join, true)
+    else
+      self.class.new(v)
+    end
   end
 
-  def eval
-    v = @t.eval.as(IntegerT).v
-    s = [] of Char
-    while v > 0
-      s << STR_TBL[v % 94]
-      v //= 94
-    end
-    StringT.new(s.reverse.join, true)
+  def to_s(io)
+    io << "$ " << @t
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "$\n"
+    @t.print(io, depth + 1)
   end
 end
 
 abstract class BinaryIntT
-  def initialize(@t0 : Term, @t1 : Term)
-  end
+  include Arity2
 
-  def eval
-    v0 = @t0.eval.as(IntegerT).v
-    v1 = @t1.eval.as(IntegerT).v
-    IntegerT.new(op(v0, v1))
+  def eval(ctx)
+    v0 = @t0.eval(ctx)
+    v1 = @t1.eval(ctx)
+    if v0.is_a?(IntegerT) && v1.is_a?(IntegerT)
+      IntegerT.new(op(v0.v, v1.v))
+    else
+      self.class.new(v0, v1)
+    end
   end
 
   abstract def op(v0, v1)
@@ -144,11 +325,31 @@ class AddT < BinaryIntT
   def op(v0, v1)
     v0 + v1
   end
+
+  def to_s(io)
+    io << "+ " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "+\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
+  end
 end
 
 class SubT < BinaryIntT
   def op(v0, v1)
     v0 - v1
+  end
+
+  def to_s(io)
+    io << "- " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "-\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
@@ -156,11 +357,31 @@ class MulT < BinaryIntT
   def op(v0, v1)
     v0 * v1
   end
+
+  def to_s(io)
+    io << "* " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "*\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
+  end
 end
 
 class DivT < BinaryIntT
   def op(v0, v1)
     v0.tdiv(v1)
+  end
+
+  def to_s(io)
+    io << "/ " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "/\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
@@ -168,121 +389,336 @@ class ModT < BinaryIntT
   def op(v0, v1)
     v0.remainder(v1)
   end
+
+  def to_s(io)
+    io << "% " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "%\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
+  end
 end
 
 class LessT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    v0 = @t0.eval(ctx)
+    v1 = @t1.eval(ctx)
+    if v0.is_a?(IntegerT) && v1.is_a?(IntegerT)
+      v0.v < v1.v ? TrueT.new : FalseT.new
+    else
+      self.class.new(v0, v1)
+    end
   end
 
-  def eval
-    v0 = @t0.eval.as(IntegerT).v
-    v1 = @t1.eval.as(IntegerT).v
-    return v0 < v1 ? TrueT.new : FalseT.new
+  def to_s(io)
+    io << "< " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "<\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class GreatT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    v0 = @t0.eval(ctx)
+    v1 = @t1.eval(ctx)
+    if v0.is_a?(IntegerT) && v1.is_a?(IntegerT)
+      v0.v > v1.v ? TrueT.new : FalseT.new
+    else
+      self.class.new(v0, v1)
+    end
   end
 
-  def eval
-    v0 = @t0.eval.as(IntegerT).v
-    v1 = @t1.eval.as(IntegerT).v
-    return v0 > v1 ? TrueT.new : FalseT.new
+  def to_s(io)
+    io << "> " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << ">\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class EqT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    v0 = @t0.eval(ctx)
+    v1 = @t1.eval(ctx)
+    if v0.is_a?(IntegerT) && v1.is_a?(IntegerT)
+      v0.v == v1.v ? TrueT.new : FalseT.new
+    elsif v0.is_a?(StringT) && v1.is_a?(StringT)
+      v0.s == v1.s ? TrueT.new : FalseT.new
+    elsif v0.is_a?(TrueT) && v1.is_a?(TrueT)
+      TrueT.new
+    elsif v0.is_a?(FalseT) && v1.is_a?(FalseT)
+      TrueT.new
+    elsif v0.is_a?(TrueT) && v1.is_a?(FalseT)
+      FalseT.new
+    elsif v0.is_a?(FalseT) && v1.is_a?(TrueT)
+      FalseT.new
+    else
+      self.class.new(v0, v1)
+    end
   end
 
-  def eval
-    case @t0
-    when IntegerT
-      return @t0.as(IntegerT).v == @t1.as(IntegerT).v ? TrueT.new : FalseT.new
-    when StringT
-      return @t0.as(StringT).s == @t1.as(StringT).s ? TrueT.new : FalseT.new
-    when TrueT
-      @t1.is_a?(TrueT) ? TrueT.new : FalseT.new
-    when FalseT
-      @t1.is_a?(FalseT) ? TrueT.new : FalseT.new
-    else
-      raise "invalid type #{@t0}"
-    end
+  def to_s(io)
+    io << "= " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "=\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class OrT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    v0 = @t0.eval(ctx)
+    v1 = @t1.eval(ctx)
+    if v0.is_a?(TrueT) || v1.is_a?(TrueT)
+      TrueT.new
+    elsif v0.is_a?(FalseT) && v1.is_a?(FalseT)
+      FalseT.new
+    else
+      self.class.new(v0, v1)
+    end
   end
 
-  def eval
-    case @t0
-    when TrueT
-      @t0
-    when FalseT
-      @t1.is_a?(TrueT) ? @t1 : @t0
-    else
-      raise "invalid type #{@t0}"
-    end
+  def to_s(io)
+    io << "| " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "|\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class AndT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    v0 = @t0.eval(ctx)
+    v1 = @t1.eval(ctx)
+    if v0.is_a?(FalseT) || v1.is_a?(FalseT)
+      FalseT.new
+    elsif v0.is_a?(TrueT) && v1.is_a?(TrueT)
+      TrueT.new
+    else
+      self.class.new(v0, v1)
+    end
   end
 
-  def eval
-    case @t0
-    when TrueT
-      @t1.is_a?(TrueT) ? @t1 : @t0
-    when FalseT
-      @t0
-    else
-      raise "invalid type #{@t0}"
-    end
+  def to_s(io)
+    io << "& " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "&\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class ConcatT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    s0 = @t0.eval(ctx)
+    s1 = @t1.eval(ctx)
+    if s0.is_a?(StringT) && s1.is_a?(StringT)
+      StringT.new(s0.s + s1.s, true)
+    else
+      self.class.new(s0, s1)
+    end
   end
 
-  def eval
-    s0 = @t0.eval.as(StringT).s
-    s1 = @t1.eval.as(StringT).s
-    return StringT.new(s0 + s1, true)
+  def to_s(io)
+    io << ". " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << ".\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class TakeT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    x = @t0.eval(ctx)
+    y = @t1.eval(ctx)
+    if x.is_a?(IntegerT) && y.is_a?(StringT)
+      StringT.new(y.s[...x.v], true)
+    else
+      self.class.new(x, y)
+    end
   end
 
-  def eval
-    x = @t0.eval.as(IntegerT).v
-    y = @t1.eval.as(StringT).s
-    return StringT.new(y[...x], true)
+  def to_s(io)
+    io << "T " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "T\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class DropT
-  def initialize(@t0 : Term, @t1 : Term)
+  include Arity2
+
+  def eval(ctx)
+    x = @t0.eval(ctx)
+    y = @t1.eval(ctx)
+    if x.is_a?(IntegerT) && y.is_a?(StringT)
+      StringT.new(y.s[x.v..], true)
+    else
+      self.class.new(x, y)
+    end
   end
 
-  def eval
-    x = @t0.eval.as(IntegerT).v
-    y = @t1.eval.as(StringT).s
-    return StringT.new(y[x..], true)
+  def to_s(io)
+    io << "D " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "D\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
+  end
+end
+
+class ApplyT
+  include Arity2
+
+  def eval(ctx)
+    puts "eval apply t0:#{@t0} t1:#{@t1}"
+    self.print(STDOUT, 0)
+    lambda = @t0
+    while !lambda.is_a?(LambdaT)
+      lambda = lambda.eval(ctx)
+    end
+    if lambda.is_a?(LambdaT)
+      lambda.apply(ctx, @t1).eval(ctx)
+    else
+      self.class.new(lambda, @t1)
+    end
+  end
+
+  def to_s(io)
+    io << "apply " << @t0 << " " << @t1
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "apply\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
   end
 end
 
 class LambdaT
-  def initialize(@t0 : Term, @t1 : Term)
+  getter :vi
+
+  def initialize(@vi : Int64, @t0 : Term)
   end
 
-  def eval
-    # TODO
+  def clone(cctx)
+    nvi = cctx.get(@vi)
+    self.class.new(nvi, @t0.clone(cctx))
+  end
+
+  def eval(ctx)
+    self
+  end
+
+  def apply(ctx, term : Term)
+    ctx.bounds[vi] = term
+    puts "apply lambda t0:#{@t0} ctx:#{ctx}"
+    self.print(STDOUT, 0)
+    ret = @t0.substitute(@vi, term, ctx)
+    ret.print(STDOUT, 0)
+    puts "ctx delete #{vi}"
+    ctx.bounds.delete(vi)
+    ret
+  end
+
+  def rename_vars(idx)
+    vi = @vi
+    idx.idx += 1
+    idx.map[vi] = idx.idx
+    @vi = idx.idx
+    @t0.rename_vars(idx)
+    idx.map.delete(vi)
+  end
+
+  def substitute(vi, term, ctx)
+    ctx.unbounds << @vi
+    @t0 = @t0.substitute(vi, term, ctx)
+    ctx.unbounds.delete(@vi)
+    self
+  end
+
+  def to_s(io)
+    io << "L[" << @vi << "] " << @t0
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "L[#{@vi}]\n"
+    @t0.print(io, depth + 1)
+  end
+end
+
+class VarT
+  def initialize(@vi : Int64)
+  end
+
+  def clone(cctx)
+    VarT.new(cctx.get(@vi))
+  end
+
+  def eval(ctx)
+    self
+  end
+
+  def rename_vars(idx)
+    @vi = idx.map.fetch(@vi, 1i64 << 30)
+  end
+
+  def substitute(vi, term, ctx)
+    if @vi == vi
+      puts "substitute var #{vi}"
+      cctx = CloneCtx.new(ctx.unbounds + ctx.bounds.keys.to_set)
+      term.clone(cctx)
+    else
+      self
+    end
+  end
+
+  def to_s(io)
+    io << "var[" << @vi << "]"
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "var[#{@vi}]\n"
   end
 end
 
@@ -290,19 +726,48 @@ class IfT
   def initialize(@t0 : Term, @t1 : Term, @t2 : Term)
   end
 
-  def eval
-    case @t0.eval
-    when TrueT
-      @t1.eval
-    when FalseT
-      @t2.eval
+  def clone(cctx)
+    self.class.new(@t0.clone(cctx), @t1.clone(cctx), @t2.clone(cctx))
+  end
+
+  def eval(ctx)
+    cond = @t0.eval(ctx)
+    if cond.is_a?(TrueT)
+      @t1.eval(ctx)
+    elsif cond.is_a?(FalseT)
+      @t2.eval(ctx)
     else
-      raise "invalid type #{@t0}"
+      self.class.new(cond, @t1, @t2)
     end
+  end
+
+  def rename_vars(idx)
+    @t0.rename_vars(idx)
+    @t1.rename_vars(idx)
+    @t2.rename_vars(idx)
+  end
+
+  def substitute(vi, term, ctx)
+    @t0 = @t0.substitute(vi, term, ctx)
+    @t1 = @t1.substitute(vi, term, ctx)
+    @t2 = @t2.substitute(vi, term, ctx)
+    self
+  end
+
+  def to_s(io)
+    io << "if " << @t0 << " " << @t1 << " " << @t2
+  end
+
+  def print(io, depth)
+    io << "  " * depth << "if\n"
+    @t0.print(io, depth + 1)
+    @t1.print(io, depth + 1)
+    @t2.print(io, depth + 1)
   end
 end
 
-alias Term = TrueT | FalseT | IntegerT | StringT | NegT | NotT | S2IT | I2ST | AddT | SubT | MulT | DivT | ModT | LessT | GreatT | EqT | OrT | AndT | ConcatT | TakeT | DropT | LambdaT | IfT
+alias PrimitiveT = TrueT | FalseT | IntegerT | StringT
+alias Term = TrueT | FalseT | IntegerT | StringT | NegT | NotT | S2IT | I2ST | AddT | SubT | MulT | DivT | ModT | LessT | GreatT | EqT | OrT | AndT | ConcatT | TakeT | DropT | ApplyT | LambdaT | VarT | IfT
 
 class Parser
   def initialize(program : String)
@@ -365,10 +830,14 @@ class Parser
       when 'D'
         return DropT.new(parse, parse)
       when '$'
-        return LambdaT.new(parse, parse)
+        return ApplyT.new(parse, parse)
       else
         raise "invalid term: #{@terms[@pos - 1]}"
       end
+    when 'L'
+      return LambdaT.new(IntegerT.parse(rest), parse)
+    when 'v'
+      return VarT.new(IntegerT.parse(rest))
     when '?'
       return IfT.new(parse, parse, parse)
     else
--- solver/src/evaluator.cr
+++ solver/src/evaluator.cr
@@ -3,7 +3,18 @@ require "common"
 def main
   parser = Parser.new(STDIN.read_line)
   prog = parser.parse
-  puts prog.eval
+  vars_idx = VarsIdx.new
+  prog.rename_vars(vars_idx)
+  puts vars_idx.idx
+  prog.print(STDOUT, 0)
+
+  while true
+    break if prog.is_a?(PrimitiveT)
+    puts "-------------------------------"
+    ctx = Ctx.new
+    prog = prog.eval(ctx)
+    prog.print(STDOUT, 0)
+  end
 end
 
 main
