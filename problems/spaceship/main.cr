b673fa2f2064f37fac068ce597edf51b9e800bb1
tomerun <tomerunmail@gmail.com>
2024-07-01 04:52:25 +0900

    extra move

--- problems/spaceship/main.cr
+++ problems/spaceship/main.cr
@@ -1,7 +1,8 @@
-RND        = Random.new
-TL         = (ENV["TL"]? || 10000).to_i
-BEAM_WIDTH = (ENV["BW"]? || 10).to_i
-LIMIT_T    = (ENV["LIMIT"]? || 10_000_000).to_i
+RND          = Random.new
+TL           = (ENV["TL"]? || 10000).to_i
+BEAM_WIDTH   = (ENV["BW"]? || 10).to_i
+TIME_FORWARD = (ENV["TF"]? || 8).to_i
+LIMIT_T      = (ENV["LIMIT"]? || 10_000_000).to_i
 alias Point = Tuple(Int32, Int32)
 DVY = [0, -1, -1, -1, 0, 0, 0, 1, 1, 1]
 DVX = [0, -1, 0, 1, -1, 0, 1, -1, 0, 1]
@@ -10,7 +11,8 @@ M2I = [
   [8, 9, 7],
   [2, 3, 1],
 ]
-TARGETS = [{0, 0}]
+TARGETS    = [{0, 0}]
+EXTRA_MEMO = Hash(Tuple(Int64, Int32), Hash(Int32, Set(Int32))).new
 
 macro debug(msg)
   {% if flag?(:local) %}
@@ -53,29 +55,68 @@ class Solver
   def solve
     beam = [State.new(0, 0, 0)]
     (TARGETS.size - 1).times do |i|
+      th_t = LIMIT_T
+      turn_start_time = Time.utc.to_unix_ms
+      EXTRA_MEMO.clear
       cy, cx = TARGETS[i]
       ny, nx = TARGETS[i + 1]
-      puts "turn:#{i}"
-      puts "(#{cy} #{cx}) -> (#{ny} #{nx})"
+      puts "turn:#{i} best_time:#{beam[0].t}"
+      # puts "(#{cy} #{cx}) -> (#{ny} #{nx})"
       hands = [] of Tuple(Int32, State)
       beam.each_with_index do |state, prev_si|
         ty = min_duration(cy, ny, state.vy)
         tx = min_duration(cx, nx, state.vx)
+        extra_y = extra(cy, ny, state.vy, {ty, th_t - state.t}.min)
+        extra_x = extra(cx, nx, state.vx, {tx, th_t - state.t}.min)
         ut = {ty, tx, 1}.max
-        next if state.t + ut > LIMIT_T
-        max_yv = max_velocity(cy, ny, state.vy, ut)
-        min_yv = min_velocity(cy, ny, state.vy, ut)
-        max_xv = max_velocity(cx, nx, state.vx, ut)
-        min_xv = min_velocity(cx, nx, state.vx, ut)
-        hands << {prev_si, State.new(state.t + ut, max_yv, max_xv)}
-        hands << {prev_si, State.new(state.t + ut, max_yv, min_xv)}
-        hands << {prev_si, State.new(state.t + ut, min_yv, max_xv)}
-        hands << {prev_si, State.new(state.t + ut, min_yv, min_xv)}
-        puts "ty:#{ty} tx:#{tx} vy:#{state.vy} vx:#{state.vx}"
-        puts "max_yv:#{max_yv}"
-        puts "min_yv:#{min_yv}"
-        puts "max_xv:#{max_xv}"
-        puts "min_xv:#{min_xv}"
+        # puts "ty:#{ty} tx:#{tx} vy:#{state.vy} vx:#{state.vx}"
+        TIME_FORWARD.times do |dt|
+          break if state.t + ut + dt > th_t
+          max_yv = max_velocity(cy, ny, state.vy, ut + dt)
+          min_yv = min_velocity(cy, ny, state.vy, ut + dt)
+          max_xv = max_velocity(cx, nx, state.vx, ut + dt)
+          min_xv = min_velocity(cx, nx, state.vx, ut + dt)
+          min_yv.upto(max_yv) do |yv|
+            min_xv.upto(max_xv) do |xv|
+              hands << {prev_si, State.new(state.t + ut + dt, yv, xv)}
+            end
+          end
+          # hands << {prev_si, State.new(state.t + ut + dt, max_yv, max_xv)}
+          # hands << {prev_si, State.new(state.t + ut + dt, max_yv, min_xv)}
+          # hands << {prev_si, State.new(state.t + ut + dt, min_yv, max_xv)}
+          # hands << {prev_si, State.new(state.t + ut + dt, min_yv, min_xv)}
+        end
+        extra_y.each do |et, evs|
+          if et >= tx
+            max_xv = max_velocity(cx, nx, state.vx, et)
+            min_xv = min_velocity(cx, nx, state.vx, et)
+            evs.each do |eyv|
+              hands << {prev_si, State.new(state.t + et, eyv, max_xv)}
+              hands << {prev_si, State.new(state.t + et, eyv, min_xv)}
+            end
+          elsif extra_x.has_key?(et)
+            evs.each do |eyv|
+              extra_x[et].each do |exv|
+                hands << {prev_si, State.new(state.t + et, eyv, exv)}
+              end
+            end
+          end
+        end
+        extra_x.each do |et, evs|
+          if et >= ty
+            max_yv = max_velocity(cy, ny, state.vy, et)
+            min_yv = min_velocity(cy, ny, state.vy, et)
+            evs.each do |exv|
+              hands << {prev_si, State.new(state.t + et, max_yv, exv)}
+              hands << {prev_si, State.new(state.t + et, min_yv, exv)}
+            end
+          end
+        end
+        # puts "max_yv:#{max_yv}"
+        # puts "min_yv:#{min_yv}"
+        # puts "max_xv:#{max_xv}"
+        # puts "min_xv:#{min_xv}"
+        th_t = hands.min_of { |h| h[1].t } + 50
       end
       next_beam = [] of State
       exist_vs = Set(Tuple(Int32, Int32)).new
@@ -111,7 +152,6 @@ class Solver
   end
 
   def recover(dist, sv, gv, t)
-    puts [dist, sv, gv, t]
     move = sv.to_i64 * t
     acc = Array.new(t, 0)
     if sv < gv
@@ -171,18 +211,69 @@ class Solver
     return acc
   end
 
-  def extra(cp, np, v)
+  def extra(cp, np, v, limit)
+    sign = 1
     if cp > np
       v *= -1
+      sign *= -1
     end
     dist = (np - cp).abs.to_i64
-    ret = [] of Tuple(Int32, Int32) # {last_v, t}
-    if v <= 0 || v.to_i64 * (v - 1) // 2 < dist
+    ret = Hash(Int32, Set(Int32)).new { |h, k| h[k] = Set(Int32).new } # {t => [last_v]}
+    if v <= 0 || v.to_i64 * (v - 1) // 2 < dist || dist == 0
       return ret
     end
-    dp = Array.new(dist + 1) { [] of Tuple(Int32, Int32) } # {v, t}
-    if dist % v == 0
+    if EXTRA_MEMO.has_key?({dist, v})
+      return EXTRA_MEMO[{dist, v}]
     end
+    dp = Array.new(dist + 1) { [] of Tuple(Int32, Int32) } # {v, t}
+    dp[dist] << {v, 0}
+    dist.downto(0) do |i|
+      dp[i].each do |cv, ct|
+        if i % cv == 0
+          # puts ["add", i, cv, ct]
+          nt = ct + i // cv
+          ret[nt] << cv * sign if nt < limit
+        end
+        {1, cv - 1}.max.upto({cv + 1, i}.min) do |nv|
+          ni = i - nv
+          found = false
+          dp[ni].size.times do |j|
+            if dp[ni][j][0] == nv && ct + 1 < limit
+              dp[ni][j] = {nv, {dp[ni][j][1], ct + 1}.min}
+              found = true
+              break
+            end
+          end
+          if !found && dp[ni].size < 5
+            dp[ni] << {nv, ct + 1}
+          end
+        end
+      end
+    end
+    # if ret.size > 5
+    #   keys = ret.keys.to_a
+    #   5.times do |i|
+    #     pos = RND.rand(keys.size - i) + i
+    #     keys.swap(i, pos)
+    #   end
+    #   keys[5..].each { |k| ret.delete(k) }
+    # end
+    # # vs_th = ret.size > 8 ? 2 : 3
+    # vs_th = 2
+    # ret.values.each do |vs|
+    #   if vs.size > vs_th
+    #     # 遅くなりすぎないように削る
+    #     tvs = vs.to_a.sort
+    #     vs.clear
+    #     vs << tvs[0] << tvs[-1]
+    #     if vs_th == 3
+    #       vs << tvs[tvs.size // 2]
+    #     end
+    #   end
+    # end
+    EXTRA_MEMO[{dist, v}] = ret
+    # puts ["extra", cp, np, v, ret]
+    return ret
   end
 
   def max_velocity(cp, np, v, t)
